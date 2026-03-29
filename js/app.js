acbfb019ce7cf90d4e4b2b31cdc2f4076cb6f3f5
Dónovan Martín <alu0101478999@ull.edu.es>
2026-03-29 21:16:46 +0100

    Init Landing Page

--- js/app.js
+++ js/app.js
@@ -2,3 +2,6 @@ import './components.js'
 import { loadLanguage } from './language.js';
 
 loadLanguage('es');
+
+
+import './carrousel.js'
---
+++ js/carrousel.js
@@ -0,0 +1,25 @@
+/* Carrusel */
+document.addEventListener('DOMContentLoaded', function () {
+  var splide = new Splide('.splide', {
+    type    : 'loop',      
+    perPage : 1,           
+    autoplay: false,       
+    
+    // Traducción de las etiquetas ARIA (Clave para nivel AA en español)
+    i18n: {
+      prev: 'Diapositiva anterior',
+      next: 'Siguiente diapositiva',
+      first: 'Ir a la primera diapositiva',
+      last: 'Ir a la última diapositiva',
+      slideX: 'Ir a la diapositiva %s',
+      pageX: 'Ir a la página %s',
+      play: 'Reproducir carrusel',
+      pause: 'Pausar carrusel',
+      carousel: 'Carrusel de nuestra flota de taxis',
+      select: 'Seleccionar una diapositiva para mostrar',
+      slideLabel: '%s de %s',
+    }
+  });
+
+  splide.mount();
+});
