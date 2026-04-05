ce7b8d8e5db1d33bc23423006ede79ce5817d7ef
Dónovan Martín <alu0101478999@ull.edu.es>
2026-04-05 13:32:18 +0100

    Corrección de error por consola global, con validación

--- js/login.js
+++ js/login.js
@@ -1,13 +1,14 @@
-// Todos los botones llevan a home.html
+if (document.querySelector(".btn-apple")) {
+    
+    document.querySelector(".btn-apple").addEventListener("click", () => {
+        window.location.href = "home.html";
+    });
 
-// document.querySelector(".btn-apple").addEventListener("click", () => {
-//   window.location.href = "home.html";
-// });
+    document.querySelector(".btn-google").addEventListener("click", () => {
+        window.location.href = "home.html";
+    });
 
-// document.querySelector(".btn-google").addEventListener("click", () => {
-//   window.location.href = "home.html";
-// });
-
-// document.querySelector(".btn-primary").addEventListener("click", () => {
-//   window.location.href = "home.html";
-// });
+    document.querySelector(".btn-primary").addEventListener("click", () => {
+        window.location.href = "home.html";
+    });
+}
